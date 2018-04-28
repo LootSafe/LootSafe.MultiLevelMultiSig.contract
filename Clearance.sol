a38d6952d48d5dbdc5e4b76643af1e38208be6f7
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-04-27 20:41:57 -0600

    Update Clearance.sol

--- Clearance.sol
+++ Clearance.sol
@@ -1,15 +1,23 @@
 
+// TODO: Actions in this file should be multisig so owner cannot have total authority
 contract Clearance {
     roles(bytes32 => Role);
     members(address => bytes32);
-    
-    // TODO: Actions in this file should be multisig so owner cannot have total authority
     
     Role {
         bytes32 id;     // ID of the role
         uint timelock;  // Length of time between 
         uint256 limit;  // Amount in wei role can withdrawl per timelock
         uint16 level;   // Ascending list of levels, e.g. level 1 can approve level 2, 3, ..., requests
+    }
+    
+    // Check that a member has the authority to authorize/deny a withdrawl
+    _isAuthorized(address _authorizor, address _requester) internal view returns (bool) {
+        Role memory authorizor = roles[members[_authorizor]];
+        Role memory requester = roles[members[_requester]];
+        
+        // Ensure authorizor has a higher ranking
+        return (authorizor.level < requester.level);
     }
     
     // Create a new role, or overwrite existing role
@@ -25,5 +33,16 @@ contract Clearance {
     // Delete a role from the system, members with this role lose all functionality
     deleteRole(bytes32 id) external onlyOwner {
         delete roles[id];
+    }
+    
+    // Assign a role to a member by address
+    assignRole(address member, bytes32 id) external onlyOwner {
+        assert(roles[id].id != 0x00);
+        members[member] = id;
+    }
+    
+    // Remove a member from a role
+    removeRole(address member) external onlyOwner {
+        delete members[member];
     }
 }
